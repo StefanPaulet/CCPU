43eadb8d845e09ba071dde64a3ddef985d2c2878
StefanPaulet <tudor.stefan.paulet@gmail.com>
2025-02-13 11:09:22 +0200

    Project setup

---
+++ src/target/main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by stefan on 9/16/24.
+//
+
+int main() { return 0; }
---
+++ test/unittest/unitTest.cpp
@@ -0,0 +1,11 @@
+//
+// Created by stefan on 7/9/24.
+//
+
+#include <catch2/catch_test_macros.hpp>
+
+auto dummyTest() -> bool { return true; }
+
+TEST_CASE("Dummy test passes", "true") {
+    REQUIRE(dummyTest() == true);
+}
